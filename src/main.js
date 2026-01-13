96cabf6eb483b9c514b4a83bf081380d649c75b9
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-01-12 17:11:09 -0800

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// Code Practice: Making a Scene
+// Name: Drullkus
+// Date: 1/12/26
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    scene: [ MainMenu, Play ]
+}
+
+let game = new Phaser.Game(config)
